03c35de150ddcf7de9dcc5eb3c463096b40234e0
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-02-03 10:19:04 +0300

    Functions

--- Python/class 1.py
+++ Python/class 1.py
@@ -1,8 +1,10 @@
-#receiving the principle
+#the function
 
-principal = int(input("Enter the principal"))
-Rate = int(input("Enter the rate"))
-time = int(input("Enter the time"))
-SI = principal*Rate*time/100
-Total = principal+SI
-print(f"The total amount is:{Total:.2f}")
+def RectangleArea(len,wid):
+    result = len*wid
+    return result
+length= int(input("Enter the length"))
+width = int(input("Enter the width"))
+
+area = length* width
+print("The area is:",area)
