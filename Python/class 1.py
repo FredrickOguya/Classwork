51b73c22023b4751f76eea9eecd1a7f504478f04
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-02-03 09:55:11 +0300

    volume

--- Python/class 1.py
+++ Python/class 1.py
@@ -1,4 +1,19 @@
-length = int(input("Kindly enter the length"))
-width = int(input("Kindly enter the width"))
-area = length * width
-print(f"the area of the rectangle is:{area}")
+#The program computes the volume and surface area
+
+pie = 3.14
+
+radius = int(input("Kindly enter the radius"))
+height = int(input("Kindly input the height of the cylinder"))
+
+#calculating the volume
+
+volume = 2 * pie * radius**2
+
+#calculating the Surface area
+
+TSA= 2*radius*pie*(radius*height)
+
+#output
+
+print(f"The volume of the cylinder is:{volume:.2f} cubic metres")
+print(f"The total surface area is: {TSA:.4f} square metres")
