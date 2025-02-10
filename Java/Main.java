5a6ce492655875b20e1dd3cf6a5149889ee6c59c
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-02-10 08:49:18 +0300

    Reference types

--- Java/Main.java
+++ Java/Main.java
@@ -1,10 +1,9 @@
+import java.util.Date;
+
 public class Main {
     public static void main (String[] args){
         byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isElligible = false;
-        System.out.println();
+        Date now = new Date();
+        System.out.println(now);
     }
 } 
