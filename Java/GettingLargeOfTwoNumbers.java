a3f084b199c7cef5de7f786a4835d5ca3a1ecf26
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-02-11 11:56:43 +0300

    Getting the large of two numbers

---
+++ Java/GettingLargeOfTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GettingLargeOfTwoNumbers {
+    public static void main(String[] arg){
+      Scanner first = new Scanner(System.in);
+      int firstNum = first.nextInt();
+      Scanner second = new Scanner(System.in);
+      int secondNum = second.nextInt();
+      if (firstNum>secondNum)
+        System.out.println(secondNum);
+        else
+        System.out.println(secondNum);
+    }
+}
