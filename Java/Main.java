49307d97ebc1992bd53f99fd46c23aee06f822fa
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-02-11 11:20:11 +0300

    variables

--- Java/Main.java
+++ Java/Main.java
@@ -4,7 +4,14 @@ import java.util.Date;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("Hello world");
-        System.out.println("Hello world2");
+        int hello_world = 5;
+        double num2 = 5.0;
+        boolean b = true;
+        char c = 'c';
+        String str = "9";
+        int tim = hello_world;
+
+        
+        System.out.println(tim);
     }
 } 
