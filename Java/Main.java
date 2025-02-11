826dffd97de9fec4909798b4c9bb898aafc3289e
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-02-11 11:43:37 +0300

    Input and scanners

--- Java/Main.java
+++ Java/Main.java
@@ -1,14 +1,11 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-       int x = 5;
-       int y = 7;
-       int z = 56;
-       double sum = x+ y-z;
-       double u = x /y;
-       double d = Math.pow(x,y);
+        Scanner sc = new Scanner(System.in);
+        String scanned = sc.next();
+        int x = Integer.parseInt(scanned);
 
-        System.out.println(d);
+        System.out.println(x);
     }
 } 
