1c2aa9b4d68ba52bbec6a80e6cb51c9e5b5e5c26
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-02-11 12:36:40 +0300

    Question one

--- Java/firstAssignmentSphereVolume.java
+++ Java/firstAssignmentSphereVolume.java
@@ -1,3 +1,9 @@
+/*Write an application that reads the radius of a sphere and prints its
+volume and surface area. Use the following formulas. Print the out-
+put to four decimal places. r represents the radius.
+Volume = 4/3πr²
+Surface area = 4πr²
+*/
 import java.util.Scanner;
 public class firstAssignmentSphereVolume {
     public static void main (String[] arg){
@@ -7,8 +13,11 @@ public class firstAssignmentSphereVolume {
         double r = radius.nextDouble();
         //calculating
         double volume = 4.0/3.0* Math.PI * Math.pow(r,3);
+        double surfaceArea = 4.0*Math.PI *Math.pow(r,2);
         
         System.out.println("The volume is");
         System.out.println(volume);
+        System.out.println("The Surface area is: ");
+        System.out.println(surfaceArea);
     }
 }
