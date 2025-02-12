f21a5e9aea4431f8957629350a21d6d13fbbba2e
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-02-12 10:58:36 +0300

    conditions and booleans

--- Java/assignment1.java
+++ Java/assignment1.java
@@ -1,5 +1,12 @@
 public class assignment1 {
     public static void main (String[]args){
-        
+        int x = 6;
+        int y = 23;
+        int z = 10;   
+        // > < == >= <= !=
+
+        boolean compare= x!=y;
+
+        System.out.println(compare);
     }
 }
