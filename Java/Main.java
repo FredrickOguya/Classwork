2a021600941c3f2a106686234d614cbccf20c952
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-02-24 11:51:53 +0300

    Hashmaps

--- Java/Main.java
+++ Java/Main.java
@@ -1,17 +1,29 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
     public static void main (String[] args){
-        map m = new HashMap();
+        Map m = new LinkedHashMap();
         m.put("tim",5);
+        m.put("joe",'x');
+        m.put("11","999");
+        m.put("a","b");
+        
+        m.containsValue("b");
+        m.containsKey(5);
 
-        System.out.println(m);
+        m.get(5);
+
+
+        System.out.println(m.isEmpty());
     }
 
         
