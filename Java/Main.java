a12ab715901807f399cafd6e2d7c8122d874c50c
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-03-10 16:36:25 +0300

    Intro to objects

--- Java/Main.java
+++ Java/Main.java
@@ -13,14 +13,27 @@ import java.util.TreeSet;
 public class Main {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        sc.next();
+        //sc.next();
         String h = "hello";
         h.length();
          
-        tim();
+        tim("Yoh!",4);
+        System.out.println(add2(6));
+        System.out.println(str("hello"));
     }
 
-    public static void tim (){
-        System.out.println("Tim");
+    public static void tim (String str, int x){
+        for(int i=0; i<x;i++){
+        System.out.println(str);
+        }
+    }
+
+    public static int add2(int x) {
+        return x+2;
+    }
+
+    public static String str(String x) {
+        return x + "!";
     }
 } 
+
