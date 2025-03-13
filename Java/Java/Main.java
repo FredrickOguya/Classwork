a0ff6e497f3fdff5404fc16df8f300388a219c73
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-03-13 21:32:02 +0300

    classes

---
+++ Java/Java/Main.java
@@ -0,0 +1,28 @@
+package Java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main (String[] args){
+        Dog tim = new Dog("tim", 4);
+        tim.speak();
+        Dog bill = new Dog("bill", 7);
+        bill.speak();
+        Dog bob = new Dog("bob", 11);
+        bob.speak();
+
+        tim.setAge(10);
+        tim.speak();
+    }
+} 
+
