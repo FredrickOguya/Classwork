471c3617c415a863b339bf8bb56b5dae27aa69cd
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-03-17 15:23:12 +0300

    today's class

---
+++ Java/NewProject.java
@@ -0,0 +1,15 @@
+public class NewProject {
+    public static void main(String[] args){
+        String name = "John";
+        Display(name);
+       System.out.println(sum(10,5,15));
+        System.out.println(largest(10,20))
+
+    }
+   
+    static int largest(int a,int b){ 
+    a>b?  return a : return b;
+    }
+
+}
+
---
+++ Python/combined.py
@@ -0,0 +1,6 @@
+import sklearn
+import pandas as pd
+import numpy as np
+
+data_df = pd.read_csv("Python/combined.csv")
+print(data_df.head())
