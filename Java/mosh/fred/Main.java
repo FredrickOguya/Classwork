6141e9f80b4a0de0a865de6b071e89d4545157f8
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 11:50:41 +0300

    Arithmetic Expressions

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,7 +1,9 @@
 package mosh.fred;
 
-public class Main{
+public class Main {
     public static void main(String[] args){
-        final float Pi = 3.14F;
+        int x = 1;
+        x+=2;
+        System.out.println(x);
     }
 }
