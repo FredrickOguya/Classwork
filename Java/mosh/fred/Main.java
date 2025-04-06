b9866f3e0e913635fd9a0f445b35fd2ff449ac77
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 11:23:45 +0300

    Escape Sequence

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,16 +1,8 @@
 package mosh.fred;
 
-public class Main {
+public class Main{
     public static void main(String[] args){
-        String message = "Hello World" + "!!";
-        
-        System.out.println(message.indexOf("e"));
-        System.out.println(message.replace("!","*"));
-        System.out.println(message.contains("Hello"));
-        System.out.println(message.toLowerCase());
-        System.out.println(message.toUpperCase());
-        System.out.println(message.trim());
+        String message = "c:\tWindows\\...";
         System.out.println(message);
-
     }
 }
