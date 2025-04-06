1aafe60353eb83755aeefd79c97e6f91de006e21
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-07 00:23:33 +0300

    For Loops

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -4,19 +4,8 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Number: ");
-        int number = scanner.nextInt();
-
-        if(number % 3 == 0 && number % 5 == 0 )
-            System.out.println("fizzBuzz");
-        
-        else if(number % 5 == 0)
-            System.out.println("Buzz");
-        else if(number% 3 == 0)
-            System.out.println("fizz");
-        else
-            System.out.println(number);
+        for(int i = 5; i > 0; i--){
+            System.out.println("Hello World " + i);
+        }
     }
 }
