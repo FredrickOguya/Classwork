49bccd778f220347c13aed103b4b4c6f9f7b4fc8
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 11:03:39 +0300

    Reference Types

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,12 +1,11 @@
 package mosh.fred;
 
+import java.util.Date;
+
 public class Main {
-    public static void main(String [] args){
+    public static void main (String[] args){
         byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-        System.out.println(age);
+        Date now = new Date();
+        System.out.println(now);
     }
 }
