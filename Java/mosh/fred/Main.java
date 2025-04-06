70945af4146ff12f26ffa548c1d2981ec1a743c6
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 11:10:53 +0300

    Primitive Types Vs Reference Type

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,11 +1,12 @@
 package mosh.fred;
 
-import java.util.Date;
+import java.awt.Point;
 
-public class Main {
-    public static void main (String[] args){
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
+public class Main{
+    public static void main(String[] args){
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
