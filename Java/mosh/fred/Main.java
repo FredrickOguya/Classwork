caf34e81f9020ffb06f6652cb2d3b74e2bd3acf4
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 11:19:38 +0300

    Strings

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,12 +1,16 @@
 package mosh.fred;
 
-import java.awt.Point;
-
-public class Main{
+public class Main {
     public static void main(String[] args){
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        String message = "Hello World" + "!!";
+        
+        System.out.println(message.indexOf("e"));
+        System.out.println(message.replace("!","*"));
+        System.out.println(message.contains("Hello"));
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
+        System.out.println(message.trim());
+        System.out.println(message);
+
     }
 }
