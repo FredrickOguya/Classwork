a96515f160cc1dcafd67c28b7a3514dda680a374
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 23:43:03 +0300

    if statements

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -2,14 +2,15 @@ package mosh.fred;
 
 public class Main{
     public static void main(String[] args){
-        int temperature =22;
-        boolean isWarm = (temperature > 20 && temperature<30);
-        System.out.println(isWarm);
-
-        boolean hasHighIncome = true;
-        boolean hasGoodCredit = true;
-        boolean hasCriminalRecord = false;
-        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+        int temp = 32;
+        if (temp > 30){
+            System.out.println("Its a hot day");
+        } 
+        else if (temp > 20 )
+            System.out.println("Beautiful day");
+        else
+            System.out.println("Cold day");
+        
 
     }
 }
