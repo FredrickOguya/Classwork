08652d2eb7fb48d1050adb61641ae56ae2cb2384
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 23:51:14 +0300

    simplifying if statements

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -2,15 +2,8 @@ package mosh.fred;
 
 public class Main{
     public static void main(String[] args){
-        int temp = 32;
-        if (temp > 30){
-            System.out.println("Its a hot day");
-        } 
-        else if (temp > 20 )
-            System.out.println("Beautiful day");
-        else
-            System.out.println("Cold day");
-        
-
+        int income = 120_000;
+        boolean hasHighIncome = (income > 100_000);
+    
     }
 }
