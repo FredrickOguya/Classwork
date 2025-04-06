ea6ad4a77f7a978f429aa51c2da508ceb7125437
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 10:56:54 +0300

    Variables

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -2,8 +2,11 @@ package mosh.fred;
 
 public class Main {
     public static void main(String [] args){
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        System.out.println(age);
     }
 }
