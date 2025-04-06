ab5bf9346f4b16137a4b76b12ae023e97bc669d2
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 11:39:37 +0300

    Multi-dimensional Arrays

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,10 +1,10 @@
 package mosh.fred;
+
 import java.util.Arrays;
-public class Main{
-    public static void main(String[] args) {
-        int[] numbers = {2,3,5,1,4};
-        Arrays.sort(numbers);
-        
-        System.out.println(Arrays.toString(numbers));
+
+public class Main {
+    public static void main(String[] args){
+        int [][] numbers = {{1,2,3},{4,5,6}};
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
