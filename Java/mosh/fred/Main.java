ef986cb8f5deca1ebdec62f50d9c82f3829e7796
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 11:42:21 +0300

    constants

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,10 +1,7 @@
 package mosh.fred;
 
-import java.util.Arrays;
-
-public class Main {
+public class Main{
     public static void main(String[] args){
-        int [][] numbers = {{1,2,3},{4,5,6}};
-        System.out.println(Arrays.deepToString(numbers));
+        final float Pi = 3.14F;
     }
 }
