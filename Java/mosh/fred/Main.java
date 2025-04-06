3d1774014f3202fd38b68aacdf261212b3dcfcf6
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 23:36:52 +0300

    Logical Operators

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -2,8 +2,14 @@ package mosh.fred;
 
 public class Main{
     public static void main(String[] args){
-        int x = 1;
-        int y = 3;
-        System.out.println(x <= y);
+        int temperature =22;
+        boolean isWarm = (temperature > 20 && temperature<30);
+        System.out.println(isWarm);
+
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+
     }
 }
