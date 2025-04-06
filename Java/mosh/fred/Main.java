c82b2f7dfc3e3e522f6954174234cb304020d24c
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 11:34:01 +0300

    arrays

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,9 +1,10 @@
 package mosh.fred;
-
+import java.util.Arrays;
 public class Main{
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[10] =3;
+        int[] numbers = {2,3,5,1,4};
+        Arrays.sort(numbers);
+        
+        System.out.println(Arrays.toString(numbers));
     }
 }
