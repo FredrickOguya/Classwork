a6715d60f77d5eefce276924bd80c8f5e1cee60e
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-06 11:26:30 +0300

    Arrays

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,8 +1,9 @@
 package mosh.fred;
 
 public class Main{
-    public static void main(String[] args){
-        String message = "c:\tWindows\\...";
-        System.out.println(message);
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[10] =3;
     }
 }
