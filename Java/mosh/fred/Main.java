a501e2bf162972680da6ba62094e9ca7799c8998
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-07 07:24:19 +0300

    for each loop

--- Java/mosh/fred/Main.java
+++ Java/mosh/fred/Main.java
@@ -1,20 +1,15 @@
 package mosh.fred;
 
-import java.util.Scanner;
-
 public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
-        while(!input.equals("quit")){
-            System.out.print("Input: ");
-            input = scanner.next().toLowerCase();
-            if (input.equals("pass"))
-                continue;
-            if (input.equals("quit"))
-                break;
-            System.out.println(input);
-            
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Mango", "Orange"};
+
+        for(int i=fruits.length-1; i>0;i--){
+            System.out.println(fruits[i]);
+        }
+
+        for (String fruit : fruits){
+            System.out.println(fruit);
         }
     }
 }
