e23b8fd15e9908c505de2c98fa3cad860c83e011
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-11 09:36:03 +0300

    global and local variables

--- Web development/Exam paractice/revision.js
+++ Web development/Exam paractice/revision.js
@@ -1,9 +1,25 @@
+//find out if the first number is larger than the second
 function ifFirstIsLarger(num1,num2){
     return num1>num2;
 };
 
+console.log(ifFirstIsLarger(40,30));
+
+
+//get the current date and time
 function getCurrentDateAndTime(){
     return new Date();
 };
-console.log(ifFirstIsLarger(40,30));
+
 console.log(getCurrentDateAndTime());
+
+//code to show example of a local and a global variable
+
+var globalVar=29;
+function myFunction(){
+    var localVar = 20;
+
+    console.log(globalVar)
+}
+console.log(globalVar);
+//console.log(localVar); error
