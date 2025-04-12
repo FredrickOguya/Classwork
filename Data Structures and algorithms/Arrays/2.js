49f220820586283a20e0a9b218c729ed87503db9
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-12 22:37:19 +0300

    my solution

---
+++ Data Structures and algorithms/Arrays/2.js
@@ -0,0 +1,11 @@
+/*
+2. Check if a Number Exists in an Array:
+	Given an array and a single number, determine if that number is present in the array.
+	Example: Array: [10, 25, 5, 15], Number to check: 5 -> True. Number to check: 20 -> False.
+*/
+
+let arr = [10, 25, 5, 15]
+
+let num = 20;
+
+console.log(arr.includes(num));
