979656f5823aea33d8e8a05a40cb572115c42ebe
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-12 23:37:55 +0300

    How i did it

--- Data Structures and algorithms/Arrays/4.js
+++ Data Structures and algorithms/Arrays/4.js
@@ -2,6 +2,8 @@
 	Given an array, modify it so that the elements are in reverse order. You should aim to do this 	without creating a new array.
     Example: [1, 2, 3, 4, 5] becomes [5, 4, 3, 2, 1]
 */
+//How to approach: Use two pointers, one starting at the beginning of the array and the other at 	the end. Swap the elements at these two pointers and then move the pointers towards the center 	until they meet or cross.
+
 let arr = [1, 2, 3, 4, 5];
 let left = 0;
 let right = arr.length -1;
