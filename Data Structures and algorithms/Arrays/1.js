926c0ab5acfc5a5eea6d5b054ddcf66bcef6065a
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-12 22:28:23 +0300

    prefered solution

--- Data Structures and algorithms/Arrays/1.js
+++ Data Structures and algorithms/Arrays/1.js
@@ -1,7 +1,7 @@
 /* Find the Minimum/Maximum Element in an Array:
 	Given an array of numbers, find the smallest and largest number in it.
 	Example: [3, 1, 4, 1, 5, 9, 2, 6] -> Minimum: 1, Maximum: 9
-*/
+
 
 let nums = [3, 1, 4, 1, 5, 9, 2, 6];
 
@@ -11,3 +11,21 @@ let minimum = ascNums[0];
 
 let maximum = ascNums[nums.length - 1];
 console.log(maximum)
+*/
+// preffered solution//
+
+let nums = [3, 1, 4, 1, 5, 9, 2, 6];
+
+let maximum = nums[0];
+let minimum = nums[0];
+
+for(let i = 0;i<nums.length;i++){
+    if(nums[i]>maximum){
+        maximum =nums[i];
+    }
+    if(nums[i]<minimum){
+        minimum =nums[i];
+    }
+}
+console.log(maximum);
+console.log(minimum);
