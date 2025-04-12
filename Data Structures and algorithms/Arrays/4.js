8b95bcf78a978eb05c9e8ad3b5a6fb6753b1aca7
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-12 23:36:53 +0300

    prefered solution

--- Data Structures and algorithms/Arrays/4.js
+++ Data Structures and algorithms/Arrays/4.js
@@ -3,5 +3,12 @@
     Example: [1, 2, 3, 4, 5] becomes [5, 4, 3, 2, 1]
 */
 let arr = [1, 2, 3, 4, 5];
+let left = 0;
+let right = arr.length -1;
 
+while(left<right){
+    [arr[left],arr[right]] = [arr[right],arr[left]];
+    left++;
+    right--;
+}
 console.log(arr)
