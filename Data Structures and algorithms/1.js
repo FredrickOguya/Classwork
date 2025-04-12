bf40668e10f6e81bcca85358ccb6df0ffa7ddaf0
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-12 22:07:58 +0300

    get first element

---
+++ Data Structures and algorithms/1.js
@@ -0,0 +1,9 @@
+//given an array of integers nums, write  function that returns the first element of the array.
+
+let nums = [1,2,3,4,5];
+
+function firstElement(arr){
+    return arr[0];
+};
+
+console.log(firstElement(nums));
