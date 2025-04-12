149cea577a2fd3154335eede93b27b4cc8718915
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-12 23:10:44 +0300

    cant do

---
+++ Data Structures and algorithms/Arrays/4.js
@@ -0,0 +1,7 @@
+/*Reverse an Array (in place):
+	Given an array, modify it so that the elements are in reverse order. You should aim to do this 	without creating a new array.
+    Example: [1, 2, 3, 4, 5] becomes [5, 4, 3, 2, 1]
+*/
+let arr = [1, 2, 3, 4, 5];
+
+console.log(arr)
