84211056f5aa8b9f92521aa019d1998a05cb01c4
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-13 21:22:51 +0300

    prefered solution

--- Data Structures and algorithms/Strings/6.js
+++ Data Structures and algorithms/Strings/6.js
@@ -3,10 +3,20 @@
 	Given a string, determine if it reads the same forwards and backward. For this easy version,
 	you can ignore spaces and case.
 	Example: "madam" -> True, "racecar" -> True, "hello" -> False
-*/
+    How to approach: Convert the string to lowercase. Compare the original string with itsreversed 	version. If they are the same, it's a palindrome.
+
+
 
 let string = "racecar";
 
 let reverse = string.split('').reverse().join('');
 
 console.log(string===reverse)
+
+*/
+
+let string = "Racecar";
+
+let reverse = string.split('').reverse().join('').toLowerCase();
+
+console.log(string.toLowerCase()===reverse)
