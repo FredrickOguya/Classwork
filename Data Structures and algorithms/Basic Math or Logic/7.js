be199a95e3e5b3033b8a8ff817c98bb477cca158
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-13 21:34:02 +0300

    preffered solution

--- Data Structures and algorithms/Basic Math or Logic/7.js
+++ Data Structures and algorithms/Basic Math or Logic/7.js
@@ -3,13 +3,19 @@ Factorial of a Number:
 	Given a non-negative integer n, calculate its factorial (the product of all positive integers less
 	than or equal to n).
 	Example: 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+    How to approach: Use a loop that iterates from 1 to n, multiplying the current result by the
+	loop counter. The factorial of 0 is 1.
 */
 
-let integer = 5;
-
-let factorial = 1;
-
-for(i=1;i<=integer;i++){
-    factorial*=i;
+function factorial(integer){
+    let factorial = 1;
+    if(integer===0){
+        return 1;
+    }
+    for(i=1;i<=integer;i++){
+        factorial*=i;
+    }
+    return factorial;
 }
-console.log(factorial);
+console.log(factorial(5));
