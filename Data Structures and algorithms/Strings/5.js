d89beb5d05389b0ebb66f143a42b8590d3a50728
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-13 21:15:46 +0300

    instructions

--- Data Structures and algorithms/Strings/5.js
+++ Data Structures and algorithms/Strings/5.js
@@ -9,7 +9,12 @@ let string = "hello";
 let reverse = string.split('').reverse().join('');
 
 console.log(reverse);
+
+//How to approach: You can iterate through the original string from the last character to the
+	first and build the reversed string character by character. Alternatively, many programming
+	languages have built-in functions or simple ways to reverse a string.
 */
+
 
 let string = "hello";
 let reverse = '';
