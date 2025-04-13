cdc8d2f3e805ebb2c78367a77ebe33d064e1dd34
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-13 21:15:02 +0300

    preffered solution

--- Data Structures and algorithms/Strings/5.js
+++ Data Structures and algorithms/Strings/5.js
@@ -2,10 +2,19 @@
 Reverse a String:
 	Given a string, create a new string that is the reverse of the original.
 	Example: "hello" -> "olleh"
-*/
+
 
 let string = "hello";
 
 let reverse = string.split('').reverse().join('');
 
+console.log(reverse);
+*/
+
+let string = "hello";
+let reverse = '';
+
+for(i=string.length-1;i>=0;i--){
+    reverse+=`${string[i]}`;
+}
 console.log(reverse);
