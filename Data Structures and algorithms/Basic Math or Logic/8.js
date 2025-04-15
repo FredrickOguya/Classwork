004f8e3f777f59c891ac9eedca736c0ef3e11f88
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-15 10:00:00 +0300

    prefer my solution

--- Data Structures and algorithms/Basic Math or Logic/8.js
+++ Data Structures and algorithms/Basic Math or Logic/8.js
@@ -2,7 +2,10 @@
 Check if a Number is Prime:
 	Given an integer greater than 1, determine if it is a prime number (only divisible by 1 and 	itself).
 	Example: 7 -> True, 10 -> False
-*/
+
+    How to approach: Iterate from 2 up to the square root of the number.
+    If the number is divisible by any integer in this range, it's not prime.
+
 
 function prime(number){
     let count = 0;
@@ -19,4 +22,15 @@ function prime(number){
     return false;
 }
 
-console.log(prime(10));
+*/
+
+function prime(number){
+    for(let i = 2;i<number;i++){
+        if(number % i ===0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(prime(7));
