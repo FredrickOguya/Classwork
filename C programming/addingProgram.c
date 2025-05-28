bc7964fd3f6ab44b9308fb6a29f7abd8c67f36a0
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-05-28 11:01:46 +0300

    simple program to add two numbers

---
+++ C programming/addingProgram.c
@@ -0,0 +1,14 @@
+/*simple program to add two numbers*/
+#include <stdio.h>
+
+int main (void)
+{
+    int v1,v2,sum;
+    v1 = 20;
+    v2 = 59;
+
+    sum = v1 + v2;
+
+    printf("The sum of %i and %i is %i\n",v1,v2,sum);
+    return 0;
+}
