6332bf0fac1be40940ef6d825a68cbffcc746d39
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-06-11 07:28:34 +0300

    while loop

---
+++ C programming/whileloop.c
@@ -0,0 +1,14 @@
+/*While to print 5 times welcome to c*/
+#include <stdio.h>
+
+void main()
+{
+    int p = 1;
+    while (p<=5)
+    {
+        /* code */
+        printf("Welcome to C\n");
+        p+=1;
+    }
+    
+}
