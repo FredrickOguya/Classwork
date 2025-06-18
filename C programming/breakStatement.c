92898e08288ae9f6c6007d4c23728995e593f7d0
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-06-18 12:08:41 +0300

    Break Statement

---
+++ C programming/breakStatement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int i = 0;
+    for(;i<6;i++)
+    {
+        
+        if(i==6)
+        {
+            break;
+        }
+        printf("%d\n",i);
+        
+    }
+}
