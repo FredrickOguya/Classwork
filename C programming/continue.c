6e579bd6f7f906b4a3bd9c7fdeb73ee94be06be5
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-06-25 12:44:00 +0300

    continue

---
+++ C programming/continue.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+    {
+        int n;
+        for(n=2; n<=9;n++)
+        {
+            if(n==4)
+            continue;
+            
+            printf("%d\n",n);
+        }
+        printf("out of loop");
+    }
